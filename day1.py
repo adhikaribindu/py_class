823bf5a36397b160f941beef339c973fe8fda7fa
Bindu <adhikaribindu208@gmail.com>
2024-01-08 10:34:37 +0545

    Add print statement

---
+++ day1.py
@@ -0,0 +1,8 @@
+print("Hello World!")
+
+name=["Shristi","Canada","Berry",2025,1.54,True,"Welcome"]
+for i in name:
+    print(i)
+    if (i=="Berry"):
+        print("Element is found")
+        break
